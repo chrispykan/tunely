3286a47b03deaf4e4aa9a627f24ce37926939f45
chrispykan <christineparisk@gmail.com>
2018-05-24 16:17:23 -0700

    ACTUALLY rendering music info

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,73 +7,67 @@
  console.log("Sanity Check: JS is working!");
 
 // hard-coded data
-var sampleAlbums = [{
-  artistName: 'Ladyhawke',
-  name: 'Ladyhawke',
-  releaseDate: '2008, November 18',
-  genres: [ 'new wave', 'indie rock', 'synth pop' ]
+const sampleAlbums = [{
+ artistName: 'Ladyhawke',
+ name: 'Ladyhawke',
+ releaseDate: '2008, November 18',
+ genres: [ 'new wave', 'indie rock', 'synth pop' ]
 }, {
-  artistName: 'The Knife',
-  name: 'Silent Shout',
-  releaseDate: '2006, February 17',
-  genres: [ 'synth pop', 'electronica', 'experimental' ]
+ artistName: 'The Knife',
+ name: 'Silent Shout',
+ releaseDate: '2006, February 17',
+ genres: [ 'synth pop', 'electronica', 'experimental' ]
 }, {
-  artistName: 'Juno Reactor',
-  name: 'Shango',
-  releaseDate: '2000, October 9',
-  genres: [ 'electronic', 'goa trance', 'tribal house' ]
+ artistName: 'Juno Reactor',
+ name: 'Shango',
+ releaseDate: '2000, October 9',
+ genres: [ 'electronic', 'goa trance', 'tribal house' ]
 }, {
-  artistName: 'Philip Wesley',
-  name: 'Dark Night of the Soul',
-  releaseDate: '2008, September 12',
-  genres: [ 'piano' ]
+ artistName: 'Philip Wesley',
+ name: 'Dark Night of the Soul',
+ releaseDate: '2008, September 12',
+ genres: [ 'piano' ]
 }];
 
 
-
 $(document).ready(function() {
-  console.log('app.js loaded!');
-  renderAlbum(sampleAlbums[0]);
+ console.log('app.js loaded!');
 });
 
 
 // this function takes a single album and renders it to the page
-function renderAlbum(album) {
-  console.log('rendering album:', album);
+sampleAlbums.forEach(function renderAlbum(album) {
+ console.log('rendering album:', album);
 
+ let albumData = `<div class="row album">
+           <div class="col-md-10 col-md-offset-1">
+             <div class="panel panel-default">
+               <div class="panel-body">
 
-
-let albumData = `<div class="row album">
-
-            <div class="col-md-10 col-md-offset-1">
-              <div class="panel panel-default">
-                <div class="panel-body">
-
-                <!-- begin album internal row -->
                   <div class='row'>
-                    <div class="col-md-3 col-xs-12 thumbnail album-art">
-                      <img src="../images/800x800.png" alt="album image">
-                    </div>
+                   <div class="col-md-3 col-xs-12 thumbnail album-art">
+                     <img src="/images/800x800.png" alt="album image">
+                   </div>
 
-                    <div class="col-md-9 col-xs-12">
-                      <ul class="list-group">
-                        <li class="list-group-item">
-                          <h4 class='inline-header'>Album Name:</h4>
-                          <span class='album-name'>${sampleAlbums.name}</span>
-                        </li>
+                   <div class="col-md-9 col-xs-12">
+                     <ul class="list-group">
+                       <li class="list-group-item">
+                         <h4 class='inline-header'>Album Name:</h4>
+                         <span class='album-name'>${album.name}</span>
+                       </li>
 
-                        <li class="list-group-item">
-                          <h4 class='inline-header'>Artist Name:</h4>
-                          <span class='artist-name'>${sampleAlbums.artistName}</span>
-                        </li>
+                       <li class="list-group-item">
+                         <h4 class='inline-header'>Artist Name:</h4>
+                         <span class='artist-name'>${album.artistName}</span>
+                       </li>
 
-                        <li class="list-group-item">
-                          <h4 class='inline-header'>Released date:</h4>
-                          <span class='album-releaseDate'>${sampleAlbums.releaseDate}</span>
-                        </li>
-                      </ul>
-                    </div> `;
+                       <li class="list-group-item">
+                         <h4 class='inline-header'>Released date:</h4>
+                         <span class='album-releaseDate'>${album.releaseDate}</span>
+                       </li>
+                     </ul>
+                   </div>`;
+ $('#albums').prepend(albumData);
+});
 
-$('#albums').prepend(albumData);
 
-}
